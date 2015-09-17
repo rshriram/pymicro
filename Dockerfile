695a53645dfba63bcac6580fedce30d444c08ca5
Shriram Rajagopalan <rshriram@gmail.com>
2015-09-17 00:24:21 +0000

    incorporate packetbeat into python microservices, log to kafka

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,21 @@ MAINTAINER Shriram Rajagopalan <rshriram@gmail.com>
 RUN apt-get update
 RUN apt-get install python-pip python-dev -y
 RUN pip install bottle json2html simplejson
-RUN apt-get install -y python-kazoo
+RUN apt-get install -y python-kazoo supervisor
+RUN mkdir -p /var/log/supervisor
+ADD packetbeat_1.0.0~beta3-custom_amd64.deb /tmp/
+#ADD packetbeat_1.0.0~beta2-kafka_amd64.deb /tmp/
+RUN dpkg -i /tmp/packetbeat_1.0.0~beta3-custom_amd64.deb; apt-get -y install -f
+#RUN dpkg -i /tmp/packetbeat_1.0.0~beta2-kafka_amd64.deb; apt-get -y install -f
+RUN rm /tmp/*.deb
+ADD packetbeat.yml /etc/packetbeat/packetbeat.yml
+ADD packetbeat-supervisor.conf /etc/supervisor/conf.d/packetbeat.conf
+
 RUN mkdir -p /opt/microservices
 ADD server.py /opt/microservices/server.py
 ADD discovery.py /opt/microservices/discovery.py
 EXPOSE 9080
-ENTRYPOINT ["/usr/bin/python", "/opt/microservices/server.py", "9080"]
+ADD pymicro-supervisor.conf /etc/supervisor/conf.d/pymicro.conf
+
+#ENTRYPOINT ["/usr/bin/python", "/opt/microservices/server.py", "9080"]
+CMD ["/usr/bin/supervisord", "-n"]
--- run_dockers.sh
+++ run_dockers.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-
-echo "starting zookeeper.."
-zoo=`docker run -d --name=zookeeper rshriram/zookeeper:3.4.6`
+echo "starting kafka"
+kafka=`docker run -d -p 2181:2181 -p 9092:9092 --name=kafka spotify/kafka`
 sleep 1
+kafkaip=`docker inspect ${kafka}|grep -i IPAddress|tr -d ' ",'|cut -d ':' -f2`
+kafkabroker="${kafkaip}:9092"
+kafkatopic="packetbeat"
+zoostr="${kafkaip}:2181"
+pushd ../kafka_2.10-0.8.2.0/
+bin/kafka-topics.sh --zookeeper ${kafkaip}:2181 --create --replication-factor 1 --partition 1 --topic ${kafkatopic}
+popd
 
-zooip=`docker inspect ${zoo}|grep -i IPAddress|tr -d ' ",'|cut -d ':' -f2`
-zoostr="${zooip}:2181"
-echo "Zookeeper ip is ${zoostr}"
-docker run -d --name=view -e ZOOKEEPER=${zoostr} -e SERVICE_NAME=view -p 9997:9080 rshriram/pymicro
+docker run -d --name=view -e ZOOKEEPER=${zoostr} -e SERVICE_NAME=view -e PACKETBEAT_KAFKA_BROKERS=${kafkabroker} -e PACKETBEAT_KAFKA_TOPIC=${kafkatopic} -p 9997:9080 rshriram/pymicro
 sleep 1
 echo "started view service"
 sleep 1
 for serv in auth serviceA serviceB serviceC serviceD serviceA1 serviceA2 serviceA3 serviceB1 serviceB2 serviceB3 serviceC1 serviceC2 serviceC3 serviceD1 serviceD2 serviceD3 serviceDB1 serviceDB2 serviceDB3
 do
-    docker run -d --name=${serv} -e ZOOKEEPER=${zoostr} -e SERVICE_NAME=${serv} rshriram/pymicro
+    docker run -d --name=${serv} -e ZOOKEEPER=${zoostr} -e SERVICE_NAME=${serv} -e PACKETBEAT_KAFKA_BROKERS=${kafkabroker} -e PACKETBEAT_KAFKA_TOPIC=${kafkatopic} rshriram/pymicro
     echo "started service ${serv}"
     sleep 1
 done
